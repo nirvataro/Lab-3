07c1ab209d101ee0ce8321dd68cf7b13135526f7
nirvataro <nvataro@gmail.com>
2021-05-07 00:46:51 +0300

    Updated Graph Class
    backjump class working, need to add improve function

--- draw_graph.py
+++ draw_graph.py
@@ -1,3 +1,13 @@
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
-
+def draw_graph(graph):
+    G = nx.DiGraph()
+    G.add_edges_from(graph.edges)
+    node_color = ['#003366', '#33cc33', '#ff0066', '#cc9900', '#cc33ff', '#00ccff', '#00ff00', '#ffcccc', '#9933ff', '#ff3300', '#4d0f00', '#737373', '#000000', '#ffff00', '#ffffcc', '#b35900', '#9900cc', '#f2ccff', '#cc0099', '#ff33cc', '#ff6666', '#1a53ff', '#99ffe6', '#00cccc']
+    color_map = []
+    for node in graph.nodes[1:]:
+        color_map.append(node_color[node.color])
+    nx.draw(G, node_color=color_map, with_labels=True)
+    plt.show()
